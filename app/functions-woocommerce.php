3c3949e3f7e164bf4eff15c30641a9de3133e3d2
David <david@shellcreeper.com>
2017-11-21 10:19:54 +0700

    Notice and redirect to setup email.

--- app/functions-woocommerce.php
+++ app/functions-woocommerce.php
@@ -61,6 +61,69 @@ function astoundify_simple_social_login_woocommerce_link_unlink_buttons() {
 add_action( 'woocommerce_after_edit_account_form', 'astoundify_simple_social_login_woocommerce_link_unlink_buttons' );
 
 /**
+ * Add Error Notice for WooCommerce
+ *
+ * @since 1.0.0
+ */
+function astoundify_simple_social_login_woocommerce_add_error_notice() {
+	// Bail if not active.
+	if ( ! astoundify_simple_social_login_is_display_location_selected( 'wp_login' ) ) {
+		return;
+	}
+	$providers = astoundify_simple_social_login_get_active_providers();
+	if ( ! $providers || ! is_array( $providers ) ) {
+		return;
+	}
+
+	// Display error
+	if ( isset( $_GET['_error'], $_GET['_provider'] ) ) {
+		$provider = astoundify_simple_social_login_get_provider( $_GET['_provider'] );
+		if ( $provider ) {
+			wc_add_notice( $provider->get_error( $_GET['_error'] ), 'error' );
+		}
+	}
+
+	// Display error if user do not have email in their account.
+	if ( is_user_logged_in() ) {
+		$user = wp_get_current_user();
+		if ( ! $user->user_email ) {
+
+			// If is not edit account page, redirect.
+			if ( ! is_edit_account_page() ) {
+				wp_safe_redirect( esc_url_raw( wc_customer_edit_account_url() ) );
+				exit;
+			} else { // Edit account page, add notice.
+				wc_add_notice( esc_html__( 'Please setup your email to activate your account.', 'astoundify-simple-social-login' ) . ' <a href="' . esc_url( wc_customer_edit_account_url() ) . '#account_email">' . esc_html__( 'Add Email', 'astoundify-simple-social-login' ) . '</a>', 'error' );
+			}
+
+		}
+	}
+}
+add_action( 'template_redirect', 'astoundify_simple_social_login_woocommerce_add_error_notice' );
+
+/**
+ * Change Setup Profile/Account Page to use WooCommerce My Account Edit Profile Page.
+ *
+ * @since 1.0.0
+ *
+ * @param string $url Profile URL.
+ * @return string
+ */
+function astoundidy_simple_social_login_woocommerce_setup_profile_url( $url ) {
+	return wc_customer_edit_account_url();
+}
+add_filter( 'astoundify_simple_social_login_setup_profile_url', 'astoundidy_simple_social_login_woocommerce_setup_profile_url' );
+
+/**
+ * Disable Redirect to WP Admin Profile Edit to Setup Email
+ *
+ * @since 1.0.0
+ *
+ * @return false
+ */
+add_filter( 'astoundify_simple_social_login_wordpress_admin_email_setup_redirect', '__return_false' );
+
+/**
  * Scritps for WooCommerce Pages.
  *
  * @since 1.0.0
@@ -86,27 +149,3 @@ function astoundify_simple_social_login_woocommerce_scripts() {
 	wp_enqueue_style( 'astoundify-simple-social-login-woocommerce', $url, array(), $version );
 }
 add_action( 'wp_enqueue_scripts', 'astoundify_simple_social_login_woocommerce_scripts' );
-
-/**
- * Add Error Notice for WooCommerce
- *
- * @since 1.0.0
- */
-function astoundify_simple_social_login_woocommerce_add_error_notice() {
-	// Bail if not active.
-	if ( ! astoundify_simple_social_login_is_display_location_selected( 'wp_login' ) ) {
-		return;
-	}
-	$providers = astoundify_simple_social_login_get_active_providers();
-	if ( ! $providers || ! is_array( $providers ) ) {
-		return;
-	}
-
-	if ( isset( $_GET['_error'], $_GET['_provider'] ) ) {
-		$provider = astoundify_simple_social_login_get_provider( $_GET['_provider'] );
-		if ( $provider ) {
-			wc_add_notice( $provider->get_error( $_GET['_error'] ), 'error' );
-		}
-	}
-}
-add_action( 'template_redirect', 'astoundify_simple_social_login_woocommerce_add_error_notice' );
